4ff8294b9ab5cef188932f1d7c045f489facdbc9
k_sougandhika <sougandhika3030@gmail.com>
2023-02-24 11:45:09 +0530

    File add

---
+++ Desktop/GitLab/firstscm/jfile.js
@@ -0,0 +1,38 @@
+pipeline {
+    agent any 
+    stages {
+        /*stage('DELETION') { 
+            steps {
+                echo '--REMOVING REPOSITORY IF ALREADY EXIST --'
+                sh "sudo rm -rf Work.js"
+            }
+        }*/
+       stage('CLONE') { 
+            steps {
+                echo '--CLONE STAGE EXECUTION ---'
+    sh "git clone git@gitlab.com:my-awesome-group8810907/firstscm.git test"
+            }
+        }
+        stage('TEST1') { 
+            steps {
+                echo '--TEST1 STAGE EXECUTION --'
+            }
+        }
+        stage('TEST2') { 
+            steps {
+                echo '--TEST2 STAGE EXECUTION --'
+            }
+        }
+        stage('BUILD') { 
+            steps {
+    echo '--BUILD STAGE EXECUTION --'
+    sh "node firstscm.jfile.js"
+            }
+        }
+  stage('DEPLOY') { 
+            steps {
+    echo '--WILL SEE IN NEXT PART --'
+            }
+        }
+    }
+}
